050446e6198fa42536a268a75ba72ebf7e13235d
Ian Chao <teriyake4@outlook.com>
2026-02-03 18:11:59 -0500

    initial framwork

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
 # Contains endpoints
+
+app = Flask(__name__)
+
+@app.post("/api/v1/ingest")
+def add_holdings():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
